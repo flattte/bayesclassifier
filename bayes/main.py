e368c55151532d55b423c2618ba0341e6c3bfd1c
flattte <79518913+flattte@users.noreply.github.com>
2022-09-03 14:25:08 +0200

    Fix newlines

--- bayes/main.py
+++ bayes/main.py
@@ -26,18 +26,15 @@ def np_implementation_iris(): #LEARNS
     train , test = split_data(data, 0.81)
     X_train, X_test, y_train, y_test = utils.label_feature_split_back(train, test, len(train[0]))
     
-
     #model fitting and predicting test set
     model = md.NaiveBayes()
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
 
-
     #print results and save them to logs
     model_log = "NaiveBayesClassifier accuracy: {}%".format(model.accuracy(y_test, y_pred)*100)
     print(model_log)
     logs.write_to_file(model_log, "iris.csv")
-
 
     #plot of class vs some features
     colnames = ["LP", "sepal lenght", "sepal width", "petalwidth", "class"]
@@ -45,8 +42,6 @@ def np_implementation_iris(): #LEARNS
     df.columns = colnames
     sns.lmplot('sepal lenght', 'sepal width', data=df, hue='class',fit_reg=False)
     plt.show()
-
-
 
 
 
@@ -60,7 +55,6 @@ def np_implementation_wine(): #LEARNS
     train , test = split_data(data, 0.77)
     X_train, X_test, y_train,  y_test = utils.label_feature_split_front(train, test, 1)
 
-
     #model fitting and predicting test set
     model = md.NaiveBayes()
     model.fit(X_train, y_train)
@@ -71,15 +65,11 @@ def np_implementation_wine(): #LEARNS
     print(model_log)
     logs.write_to_file(model_log, "wine.data")
 
-
-    
     #1,3
     classify = [[14.06,2.15,2.61,17.6,121,2.6,2.51,.31,1.25,5.05,1.06,3.58,1295],
                 [12.96,3.45,2.35,18.5,106,1.39,.7,.4,.94,5.28,.68,1.75,675]]
     pred = model.predict(classify)
     print(pred)
-
-
 
 
 def glass_implementation():   #LEARNS
@@ -92,12 +82,10 @@ def glass_implementation():   #LEARNS
     train, test = split_data(data, 0.80)
     X_train, X_test, y_train, y_test = utils.label_feature_split_back(train, test, len(train[0]))
 
-
     #model fitting and predicting test set
     model = md.NaiveBayes()
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
-
 
     #print results and save them to logs
     model_log = "NaiveBayesClassifier accuracy: {}%".format(model.accuracy(y_test, y_pred)*100)
@@ -109,8 +97,6 @@ def glass_implementation():   #LEARNS
                 [194,1.51625,13.36,3.58,1.49,72.72,0.45,8.21,0,0]]
     pred = model.predict(classify)
     print(pred)
-
-
     
     #plot of class vs some features
     columns= ['LP','RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe','class']
@@ -118,9 +104,6 @@ def glass_implementation():   #LEARNS
     df.columns = columns
     sns.lmplot('K', 'Al', data=df, hue='class',fit_reg=False)
     plt.show()
-
-
-
 
 
 
@@ -139,7 +122,6 @@ def job_implementation(): #NEEDS WORK
     df['occupation'].replace('?', np.NaN, inplace=True)
     df['native_country'].replace('?', np.NaN, inplace=True)
     #todo
-
 
 
 if __name__ == '__main__':
